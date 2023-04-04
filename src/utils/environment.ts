1d9c1dcfc111e2bd43f0cd28b99d97899fd6714d
mryhryki <mryhryki@gmail.com>
2023-04-04 16:20:58 +0900

    Update src/utils/environment.ts

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,7 +2,7 @@ const Environments = ['production', 'local'] as const
 type Environment = typeof Environments[number]
 
 export const getEnvironment = (): Environment => {
-  switch (process.env.ENVIRONMENT) {
+  switch (process.env.NEXT_PUBLIC_ENVIRONMENT) {
     case 'local':
       return 'local'
     default:
